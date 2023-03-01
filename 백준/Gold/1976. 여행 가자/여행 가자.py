cf696b08050001876bf001b25b90baff9c7af581
cat-on-laptop <95871630+cat-on-laptop@users.noreply.github.com>
2023-03-02 01:14:17 +0900

    [Gold IV] Title: 여행 가자, Time: 132 ms, Memory: 115372 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,43 @@
+import sys
+si = sys.stdin.readline
+
+n, m = int(si()), int(si())
+
+parent = [i for i in range(n + 1)]
+rank = [0] * (n + 1)
+
+def find(x : int) -> int:
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x : int, y : int) -> None:
+    # x를 y의 밑으로
+    px, py = find(x), find(y)
+    
+    if px == py: return
+
+    if rank[px] < rank[py]:
+        parent[px] = py
+    elif rank[px] > rank[py]:
+        parent[py] = px
+    else :
+        parent[px] = py
+        rank[py] += 1
+
+for i in range(1, n + 1):
+    arr = list(map(int, si().split()))
+    for j in range(n):
+        if arr[j] == 1:
+            union(i, j + 1)
+
+plan = list(map(int, si().split()))
+
+flag = True
+for i in range(1, m):
+    if find(plan[i-1]) != find(plan[i]):
+        flag = False
+        break
+
+print("YES" if flag else "NO")
