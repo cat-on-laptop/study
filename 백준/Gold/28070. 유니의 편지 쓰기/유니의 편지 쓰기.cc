7661359dc0f096bcb4b1774acea9b0ec7426ffd5
cat-on-laptop <95871630+cat-on-laptop@users.noreply.github.com>
2023-06-18 20:56:13 +0900

    [Gold V] Title: 유니의 편지 쓰기, Time: 132 ms, Memory: 2528 KB -BaekjoonHub

---
+++ 백준/Gold/28070. 유니의 편지 쓰기/유니의 편지 쓰기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int imos[10000][13];
+int y, m, ans;
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; ++i) {
+        char buff; int x, y, z, w;
+        cin >> x >> buff >> y  >> z >> buff >> w;
+        ++imos[x][y];
+        if(w == 12) --imos[z+1][1];
+        else --imos[z][w+1];
+    }
+
+    for(int i = 2000; i <= 9999; ++i) {
+        for(int j = 0; j <= 12; ++j) {
+            if(j == 0) imos[i][j] += imos[i-1][12];
+            else imos[i][j] += imos[i][j-1];
+        }
+    }
+    
+    for(int i = 2000; i <= 9999; ++i) 
+        for(int j = 0; j <= 12; ++j) {
+            if(ans < imos[i][j]) {
+                y = i, m = j, ans = imos[i][j];
+            }
+        }
+
+    cout << y << '-';
+    if(m < 10) cout << 0;
+    cout << m << endl;
+}
