4414f242938bebcc45faf5e0d51dc3b9b5728e07
cat-on-laptop <95871630+cat-on-laptop@users.noreply.github.com>
2023-07-04 11:30:51 +0900

    [D2] Title: 두 개의 숫자열, Time: 7 ms, Memory: 13,376 KB -BaekjoonHub

---
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[25], b[25];
+
+int main() {
+    int t; cin >> t;
+    for(int tc = 1; tc <= t; ++tc) {
+        int n, m; cin >> n >> m;
+
+        for(int i = 0; i < n; ++i) cin >> a[i];
+        for(int j = 0; j < m; ++j) cin >> b[j];
+
+        int ans = 0, temp;
+
+        if(n < m) {
+            for(int i = 0; i + n <= m; ++i) {
+                temp = 0;
+                for(int j = 0; j < n; ++j) {
+                    temp += a[j] * b[i+j];
+                }
+                ans = max(ans, temp);
+            }
+        }
+        else {
+            for(int i = 0; i + m <= n; ++i) {
+                temp = 0;
+                for(int j = 0; j < m; ++j) {
+                    temp += a[i+j] * b[j];
+                }
+                ans = max(ans, temp);
+            }
+        }
+        cout << '#' << tc << ' ' << ans << endl;
+    }
+
+    return 0;
+}
