854f213ed32c52f0d5683f934a11001dda291f35
cat-on-laptop <95871630+cat-on-laptop@users.noreply.github.com>
2023-07-04 21:25:07 +0900

    [D2] Title: 스도쿠 검증, Time: 7 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.cpp
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-
+ 
 using namespace std;
-
+ 
 int arr[9][9];
 int check;
-
+ 
 int main() {
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
-    
+     
     int t; cin >> t;
     for(int tc = 1; tc <= t; ++tc) {
         for(int i = 0; i < 9; ++i) 
             for(int j = 0; j < 9; ++j) 
                 cin >> arr[i][j];
-        
+         
         int ans = 1;
-
+ 
         // 행 단위로 검증
         for(int row = 0; row < 9; ++row) {
             check = 0;
@@ -27,9 +27,9 @@ int main() {
             for(int num = 1; num <= 9; ++num) 
                 if(!(check & (1 << num))) ans = 0;
         }
-
+ 
         // cout << "after row " << ans << endl;
-
+ 
         // 열 단위로 검증
         for(int col = 0; col < 9; ++col) {
             check = 0;
@@ -40,9 +40,9 @@ int main() {
             for(int num = 1; num <= 9; ++num)
                 if(!(check * (1 << num))) ans = 0;
         }
-
+ 
         // cout << "after col " << ans << endl;
-
+ 
         // 각 사각형 단위로 검증
         for(int row = 0; row < 3; ++row) {
             for(int col = 0; col < 3; ++col) {
@@ -57,11 +57,11 @@ int main() {
                     if(!(check & (1 << num))) ans = 0;
             }
         }
-
+ 
         // cout << "after square " << ans << endl;
-
+ 
         cout << '#' << tc << ' ' << ans << endl;
     }
-
+ 
     return 0;
 }
