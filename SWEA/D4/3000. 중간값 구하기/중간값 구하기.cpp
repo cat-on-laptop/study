2167e1db0effe3c8378ffa4db65b956d040a8b14
cat-on-laptop <95871630+cat-on-laptop@users.noreply.github.com>
2023-09-04 11:49:27 +0900

    [D4] Title: 중간값 구하기, Time: 3,727 ms, Memory: 17,088 KB -BaekjoonHub

--- SWEA/D4/3000. 중간값 구하기/중간값 구하기.cpp
+++ SWEA/D4/3000. 중간값 구하기/중간값 구하기.cpp
@@ -1,42 +1,127 @@
 #include <iostream>
-#include <queue>
-#include <functional>
 
-using namespace std;
+using std::cin;
+using std::cout;
 
+int n, minCnt, maxCnt;
+int maxHeap[200005];
+int minHeap[200005];
 const int mod = 20171109;
-priority_queue<int, vector<int>, greater<int>> big;
-priority_queue<int, vector<int>, less<int>> small;
-int ans, n;
+
+int min_top() {
+	return minHeap[1];
+}
+
+int max_top() {
+	return maxHeap[1];
+}
+
+void min_push(int val) {
+	minHeap[++minCnt] = val;
+	int idx = minCnt, temp;
+	while (idx > 1) {
+		if (minHeap[idx >> 1] > minHeap[idx]) {
+			temp = minHeap[idx >> 1];
+			minHeap[idx >> 1] = minHeap[idx];
+			minHeap[idx] = temp;
+			idx >>= 1;
+		}
+		else break;
+	}
+}
+
+void max_push(int val) {
+	maxHeap[++maxCnt] = val;
+	int idx = maxCnt, temp;
+	while (idx > 1) {
+		if (maxHeap[idx >> 1] < maxHeap[idx]) {
+			temp = maxHeap[idx >> 1];
+			maxHeap[idx >> 1] = maxHeap[idx];
+			maxHeap[idx] = temp;
+			idx >>= 1;
+		}
+		else break;
+	}
+}
+
+void min_pop() {
+	minHeap[1] = minHeap[minCnt--];
+
+	int curr = 1;
+	while ((curr << 1) <= minCnt) { // 자식이 있다면
+		int child;
+		
+		if ((curr << 1) == minCnt) // 왼쪽 자식만 있다면
+			child = curr << 1;
+		else // 둘 중 더 작은 것을 선택해야 minHeap 성질 유지
+			child = minHeap[(curr << 1)] < minHeap[(curr << 1) + 1] ? curr << 1 : (curr << 1) + 1;
+
+		if (minHeap[curr] < minHeap[child]) break;
+
+		int temp = minHeap[child];
+		minHeap[child] = minHeap[curr];
+		minHeap[curr] = temp;
+		curr = child;
+	}
+}
+
+void max_pop() {
+	maxHeap[1] = maxHeap[maxCnt--];
+
+	int curr = 1;
+	while ((curr << 1) <= maxCnt) { // 자식이 있다면
+		int child;
+
+		if ((curr << 1) == maxCnt) // 왼쪽 자식만 있다면
+			child = curr << 1;
+		else // 둘 중 더 작은 것을 선택해야 minHeap 성질 유지
+			child = maxHeap[(curr << 1)] > maxHeap[(curr << 1) + 1] ? curr << 1 : (curr << 1) + 1;
+
+		if (maxHeap[curr] > maxHeap[child]) break;
+
+		int temp = maxHeap[child];
+		maxHeap[child] = maxHeap[curr];
+		maxHeap[curr] = temp;
+		curr = child;
+	}
+}
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
+	std::ios::sync_with_stdio(0), cin.tie(0);
+	int t; cin >> t;
 
-    int t; cin >> t;
-    for(int tc = 1; tc <= t; ++tc) {
-        big = priority_queue<int, vector<int>, greater<int>>();
-        small = priority_queue<int, vector<int>, less<int>>();
-        ans = 0;
-        int val;
-        // small이 하나 더 많게
-        cin >> n >> val;
+	for (int tc = 1; tc <= t; ++tc) {
+		// init
+		int ans = 0;
+		minCnt = maxCnt = 0;
+		cin >> n;
 
-        small.push(val);
+		int x, y;
+		cin >> x;
+		max_push(x);
 
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < 2; ++j) {
-                cin >> val;
-                if(small.size() == big.size()) { // size가 같으면 small에 넣어줘야 함
-                    big.push(val); small.push(big.top()); big.pop();
-                }
-                else {
-                    small.push(val); big.push(small.top()); small.pop();
-                }
-            }
-            ans += small.top();
-            ans %= mod;
-        }
+		for (int i = 0; i < n; ++i) {
+			cin >> x >> y;
+			if (x > y) {
+				min_push(x);
+				max_push(y);
+			}
+			else {
+				min_push(y);
+				max_push(x);
+			}
 
-        cout << "#" << tc << ' ' << ans << '\n';
-    }
+			while (min_top() < max_top()) {
+				int min = min_top();
+				int max = max_top();
+				min_pop(); max_pop();
+				min_push(max);
+				max_push(min);
+			}
+
+			ans = (ans + max_top()) % mod;
+
+		}
+		cout << '#' << tc << ' ' << ans << '\n';
+	}
 }
