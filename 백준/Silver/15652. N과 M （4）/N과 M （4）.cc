007d8f658ff63d9784140b9c7008696ffea4c2d4
sunsetcat <95871630+cat-on-laptop@users.noreply.github.com>
2023-09-10 01:26:15 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int arr[15];
+int n, m;
+
+void f(int curr, int idx) {
+	if(curr == m) {
+		for(int i = 0; i < m; ++i) cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for(int i = idx; i <= n; ++i) {
+		arr[curr] = i;
+		f(curr + 1, i);
+	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(0), cin.tie(0);
+	// input
+	cin >> n >> m;
+
+	f(0, 1);
+}
