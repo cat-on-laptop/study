58dc040f8560bc8c276e73b345928a5f1d31c05b
sunsetcat <95871630+cat-on-laptop@users.noreply.github.com>
2023-09-12 23:57:05 +0900

    [Gold I] Title: 본대 산책2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12850. 본대 산책2/본대 산책2.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#define MOD 1'000'000'007
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+typedef vector<vector<long long>> matrix_t;
+
+matrix_t arr = {
+	{0,1,1,0,0,0,0,0},
+	{1,0,1,1,0,0,0,0},
+	{1,1,0,1,1,0,0,0},
+	{0,1,1,0,1,1,0,0},
+	{0,0,1,1,0,1,1,0},
+	{0,0,0,1,1,0,0,1},
+	{0,0,0,0,1,0,0,1},
+	{0,0,0,0,0,1,1,0}
+};
+
+matrix_t ans = vector<vector<long long>>(8, vector<long long>(8, 0));
+
+matrix_t operator*(const matrix_t& x, const matrix_t& y) {
+	matrix_t rst = vector<vector<long long>>(8, vector<long long>(8, 0));
+
+	for (int i = 0; i < 8; ++i) {
+		for (int j = 0; j < 8; ++j) {
+			for (int k = 0; k < 8; ++k) {
+				rst[i][j] += x[i][k] * y[j][k];
+				rst[i][j] %= MOD;
+			}
+		}
+	}
+
+	return rst;
+}
+
+int main() {
+	for (int i = 0; i < 8; ++i) ans[i][i] = 1;
+	int n; cin >> n;
+
+	while (n > 0) {
+		if (n & 1) ans = ans * arr;
+
+		arr = arr * arr;
+		n >>= 1;
+	}
+
+	cout << ans[0][0];
+}
