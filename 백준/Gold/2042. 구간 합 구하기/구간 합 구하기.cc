c3ae382def1dbf722f42cd8469e37c9544480c38
sunsetcat <95871630+cat-on-laptop@users.noreply.github.com>
2023-10-08 16:27:42 +0900

    [Gold I] Title: 구간 합 구하기, Time: 228 ms, Memory: 41088 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,59 +1,73 @@
 #include <iostream>
-#define MAX 1024
-#define endl '\n'
-using namespace std;
+#include <vector>
 
+using std::cin;
+using std::cout;
+using std::vector;
 typedef long long ll;
 
-ll tree[(MAX*MAX) << 1];
 int n, m, k;
-int tempN;
+vector<ll> tree;
+vector<ll> arr;
 
-void update(int idx, ll val){
-    idx = idx + tempN - 1;
-    tree[idx] = val;
-    idx = idx >> 1;
-    while(idx >= 1){
-        tree[idx] = tree[idx << 1] + tree[(idx << 1) + 1];
-        idx = idx >> 1;
+void init(int node, int nodeLeft, int nodeRight) {
+    if(nodeLeft == nodeRight) {
+        tree[node] = arr[nodeLeft];
+        return;
     }
+
+    int mid = (nodeLeft + nodeRight) >> 1;
+    init(node << 1, nodeLeft, mid);
+    init((node << 1) + 1, mid + 1, nodeRight);
+
+    tree[node] = tree[node << 1] + tree[(node << 1) + 1];
 }
 
-ll sum(int a, int b){
-    ll val = 0;
-    a = a + tempN - 1;
-    b = b + tempN - 1;
-    while(a <= b){
-        if(a & 1) val += tree[a];
-        if(!(b & 1)) val += tree[b];
-        a = (a + 1) >> 1;
-        b = (b - 1) >> 1;
+void update(int node, int nodeLeft, int nodeRight, int idx, ll val) {
+    if (idx < nodeLeft || nodeRight < idx) return;
+
+    if(nodeLeft == nodeRight) {
+        tree[node] = val;
+        return;
     }
-    return val;
+
+    int mid = (nodeLeft + nodeRight) >> 1;
+    update(node << 1, nodeLeft, mid, idx, val);
+    update((node << 1) + 1, mid + 1, nodeRight, idx, val);
+
+    tree[node] = tree[node << 1] + tree[(node << 1) + 1];
 }
 
-int main(){
-    ios::sync_with_stdio(0), cin.tie(0);
+ll query(int node, int nodeLeft, int nodeRight, int queryLeft, int queryRight) {
+    if (queryRight < nodeLeft || nodeRight < queryLeft) return 0;
+
+    if(queryLeft <= nodeLeft && nodeRight <= queryRight) return tree[node];
+
+    int mid = (nodeLeft + nodeRight) >> 1;
+
+    ll left = query(node << 1, nodeLeft, mid, queryLeft, queryRight);
+    ll right = query((node << 1) + 1, mid + 1, nodeRight, queryLeft, queryRight);
+
+    return left + right;
+}
+
+int main() {
+    std::ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> m >> k;
+    tree.resize(n << 2);
+    arr.resize(n);
 
-    // n 이상인 2의 거듭제곱 tempN 
-    for(tempN = 1; tempN < n; tempN  = tempN << 1);
-    
-    for(int i = 0; i < n; ++i) cin >> tree[tempN + i];
-    for(int i = tempN - 1; i >= 1; --i) tree[i] = tree[i << 1] + tree[(i << 1) + 1];
+    for(int i = 0; i < n; ++i)
+        cin >> arr[i];
+    init(1, 0, n-1);
 
-    int a, b;
-    ll c;
-    m += k;
-    for(int q = 0; q < m; ++q){
-        cin >> a >> b >> c;
-        if(a == 1){
-            update(b, c);
-        }
-        else{
-            cout << sum(b, c) << endl;
-        }
-    }
+    int op;
+    ll a, b;
 
+    for(int i = 0; i < m + k; ++i) {
+        cin >> op >> a >> b;
+        if(op & 1) update(1,0,n-1,a-1,b); 
+        else cout << query(1,0,n-1,a-1,b-1) << '\n';
+    }   
     return 0;
 }
