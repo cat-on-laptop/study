c70690e9aef7721f0f0071aff0d45c850df128c9
sunsetcat <95871630+cat-on-laptop@users.noreply.github.com>
2023-11-07 14:39:56 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 0 ms, Memory: 5148 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#define INF 0x7fffffffffffffff
+
+using std::cin;
+using std::cout;
+using std::priority_queue;
+using std::vector;
+
+typedef long long ll;
+
+struct edge_t {
+	int node;
+	ll weight;
+
+	bool operator< (const edge_t& e) const {
+		if (weight > e.weight) return true;
+		return false;
+	}
+};
+
+int n, m, a, b, val;
+ll c;
+ll dist[100'005];
+
+vector<edge_t> arr[100'005];
+
+inline int max(int x, int y) {
+	return x > y ? x : y;
+}
+
+bool dijkstra(int offset) {
+	for (int i = 1; i <= n; ++i)
+		dist[i] = INF;
+
+	dist[a] = 0;
+	priority_queue<edge_t> pq;
+	pq.push({ a,0 });
+
+	while (!pq.empty()) {
+		edge_t curr = pq.top(); pq.pop();
+
+		if (dist[curr.node] < curr.weight) continue;
+
+		for (const edge_t& e : arr[curr.node]) {
+			if (e.weight > offset)
+				continue;
+
+			if (dist[e.node] <= dist[curr.node] + e.weight)
+				continue;
+
+			dist[e.node] = dist[curr.node] + e.weight;
+			pq.push({ e.node, dist[e.node] });
+		}
+	}
+
+	return dist[b] <= c;
+}
+
+int main() {
+	std::ios::sync_with_stdio(0), cin.tie(0);
+	// freopen("input.txt", "r", stdin);
+	cin >> n >> m >> a >> b >> c;
+
+	int x, y, z;
+
+	for (int i = 0; i < m; ++i) {
+		cin >> x >> y >> z;
+		arr[x].push_back({ y,z });
+		arr[y].push_back({ x,z });
+		val = max(val, z + 1);
+	}
+
+	int l = 1, r = val, m;
+
+	// fffffffffffttttttttt 인 첫 t를 찾는 이분 탐색
+	// 구간 [l, r)에 대하여 탐색
+
+	while (l != r) {
+		m = l + ((r - l) >> 1);
+		dijkstra(m) ? r = m : l = m + 1;
+	}
+
+	r == val ? cout << -1 : cout << l;
+
+	return 0;
+}
