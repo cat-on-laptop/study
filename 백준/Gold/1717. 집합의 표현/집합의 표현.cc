8a5694b17bae9e8caf934a7d5d7cab700d92215a
sunsetcat <95871630+cat-on-laptop@users.noreply.github.com>
2024-01-17 01:18:57 +0900

    [Gold V] Title: 집합의 표현, Time: 40 ms, Memory: 6320 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,35 +1,40 @@
 #include <iostream>
-#define MAX 1'000'005
-using namespace std;
 
-int parent[MAX], n, m;
+using std::cin;
+using std::cout;
 
-int find(int a){
-    // 결국 타고 올라가는 건 find니 find에서 경로 압축
-    if(parent[a] == a) return a;
-    else return parent[a] = find(parent[a]);
+int parent[100'000'5];
+
+int find(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
 }
 
-void Union(int a, int b){
-    // a를 b로
-    int pA = find(a), pB = find(b);
-    parent[pA] = pB;
+void merge(int x, int y) {
+    int px = find(x), py = find(y);
+
+    if(px == py) return;
+
+    parent[px] = py;
 }
 
-int main(){
-    ios::sync_with_stdio(0), cin.tie(0);
+int main() {
+    std::ios::sync_with_stdio(0), cin.tie(0);
+
+    int n, m;
 
     cin >> n >> m;
+
     for(int i = 0; i <= n; ++i) parent[i] = i;
-    int op, a, b;
-    for(int i = 0; i < m; ++i){
+
+    for(int i = 0; i < m; ++i) {
+        int op, a, b;
         cin >> op >> a >> b;
-        if(op){
+
+        if(!op) merge(a, b);
+        else {
             if(find(a) == find(b)) cout << "YES\n";
             else cout << "NO\n";
-        }
-        else{
-            Union(a, b);
         }
     }
     return 0;
